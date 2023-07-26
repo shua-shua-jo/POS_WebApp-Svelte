d51488b16254ef882381f95bfc33842a3e3c4cd1
Josh <joshuaabello02@gmail.com>
2023-07-27 03:26:06 +0800

    changed target to blank

--- src/routes/(user)/+layout.svelte
+++ src/routes/(user)/+layout.svelte
@@ -43,7 +43,7 @@
 			</ul>
 		</div>
 		<div class="nav-request">
-			<a class="nav-link-req" href="/request-forms" data-sveltekit-reload
+			<a class="nav-link-req" href="/request-forms" target="_blank"
 				><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
 					><path
 						fill="var(--upcolor_maroon)"
