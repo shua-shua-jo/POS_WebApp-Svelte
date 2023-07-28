c83069d8fd8a5e5a713494b8a2b384ae62965bcf
Josh <joshuaabello02@gmail.com>
2023-07-28 17:39:36 +0800

    added lists

---
+++ src/lib/server/lists.js
@@ -0,0 +1,51 @@
+export const requirements = new Map([
+	[
+		'Honorable Dismissal',
+		[
+			'Letter from parents stating the reason for Honorable Dismissal',
+			'Signed letter stating the reason for Honorable Dismissal',
+			'Latest University Clearance',
+			'Exit counseling from the Office of Counseling and Guidance',
+			'Official Transcript of Records'
+		]
+	]
+]);
+
+export const formData = new Map([
+	[
+		'documents1',
+		[
+			{
+				'Honorable Dismissal': { price: 220, scholarship: false }
+			},
+			{ 'Certified True Text of Diploma': { price: 20, scholarship: false } },
+			{ 'Diploma Translation': { price: 50, scholarship: false } },
+			{ 'Certified True Copy of Diploma': { price: 220, scholarship: false } },
+			{ 'Course Description': { price: 20, scholarship: false } },
+			{ 'True Copy of Grades': { price: 50, scholarship: true } },
+			{ 'Certificate of Enrollment': { price: 50, scholarship: true } },
+			{ 'Transfer to Other UP Units': { price: 150, scholarship: false } },
+			{ 'Certificate of Non-Issuance of ID': { price: 50, scholarship: true } },
+			{ 'Official Transcript of Records': { price: 70, scholarship: false } }
+		]
+	],
+
+	[
+		'documents2',
+		[
+			{ 'Certificate of Graduation': { price: 50, scholarship: false } },
+			{ 'Certificate of No Disciplinary Action': { price: 50, scholarship: true } },
+			{ 'Certificate of No Contract': { price: 50, scholarship: false } },
+			{ 'Certificate of Units Earned': { price: 50, scholarship: false } },
+			{ 'Certificate of Medium of Instruction': { price: 50, scholarship: false } },
+			{ 'Certificate of Grade Equivalence': { price: 50, scholarship: false } },
+			{
+				'Certificate of Non-Issuance of Honorable Dismissal for Graduates': {
+					price: 50,
+					scholarship: false
+				}
+			},
+			{ 'Certificate of Non-Issuance of S.O. Number': { price: 50, scholarship: false } }
+		]
+	]
+]);
