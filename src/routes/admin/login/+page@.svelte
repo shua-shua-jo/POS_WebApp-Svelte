8f4c58620f7c9a8e3dadf627f49f7300d29dbcfc
Josh <joshuaabello02@gmail.com>
2023-07-30 18:39:31 +0800

    added show password feature

--- src/routes/admin/login/+page@.svelte
+++ src/routes/admin/login/+page@.svelte
@@ -2,6 +2,8 @@
 	import up2gologo_colored from '$lib/images/logos/up2go-colored.png';
 	import uplogo from '$lib/images/logos/uplogo.png';
 	import login_bg1 from '$lib/images/bg/login-bg1.jpg';
+
+	let showPass = false;
 </script>
 
 <section class="login-page">
@@ -49,12 +51,45 @@
 				>
 				<input
 					class="admin_password"
-					type="password"
+					type={!showPass ? 'password' : 'text'}
 					name="password"
 					placeholder="Enter your password"
 					autocomplete="off"
 					required
 				/>
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				{#if showPass}
+					<svg
+						class="togglePass"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						on:click={() => {
+							showPass = !showPass;
+						}}
+						><path
+							fill="currentColor"
+							d="M12 16q1.875 0 3.188-1.313T16.5 11.5q0-1.875-1.313-3.188T12 7q-1.875 0-3.188 1.313T7.5 11.5q0 1.875 1.313 3.188T12 16Zm0-1.8q-1.125 0-1.913-.788T9.3 11.5q0-1.125.788-1.913T12 8.8q1.125 0 1.913.788T14.7 11.5q0 1.125-.787 1.913T12 14.2Zm0 4.8q-3.475 0-6.35-1.838T1.3 12.2q-.075-.125-.1-.312t-.025-.388q0-.2.025-.388t.1-.312q1.475-3.125 4.35-4.963T12 4q3.475 0 6.35 1.838T22.7 10.8q.075.125.1.313t.025.387q0 .2-.025.388t-.1.312q-1.475 3.125-4.35 4.963T12 19Zm0-7.5Zm0 5.5q2.825 0 5.188-1.488T20.8 11.5q-1.25-2.525-3.613-4.013T12 6Q9.175 6 6.812 7.488T3.2 11.5q1.25 2.525 3.613 4.013T12 17Z"
+						/></svg
+					>
+				{:else}
+					<svg
+						class="togglePass"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						on:click={() => {
+							showPass = !showPass;
+						}}
+						><path
+							fill="currentColor"
+							d="m16.1 13.3l-1.45-1.45q.225-1.175-.675-2.2t-2.325-.8L10.2 7.4q.425-.2.863-.3T12 7q1.875 0 3.188 1.313T16.5 11.5q0 .5-.1.938t-.3.862Zm3.2 3.15l-1.45-1.4q.95-.725 1.688-1.587T20.8 11.5q-1.25-2.525-3.588-4.013T12 6q-.725 0-1.425.1T9.2 6.4L7.65 4.85q1.025-.425 2.1-.638T12 4q3.575 0 6.425 1.887T22.7 10.8q.075.125.1.313t.025.387q0 .2-.037.388t-.088.312q-.575 1.275-1.437 2.35t-1.963 1.9Zm-.2 5.45l-3.5-3.45q-.875.275-1.762.413T12 19q-3.575 0-6.425-1.888T1.3 12.2q-.075-.125-.1-.312t-.025-.388q0-.2.025-.375t.1-.3Q1.825 9.7 2.55 8.75T4.15 7L2.075 4.9Q1.8 4.625 1.8 4.212t.3-.712q.275-.275.7-.275t.7.275l17 17q.275.275.288.688t-.288.712q-.275.275-.7.275t-.7-.275ZM5.55 8.4q-.725.65-1.325 1.425T3.2 11.5q1.25 2.525 3.588 4.013T12 17q.5 0 .975-.063t.975-.137l-.9-.95q-.275.075-.525.113T12 16q-1.875 0-3.188-1.312T7.5 11.5q0-.275.038-.525t.112-.525L5.55 8.4Zm7.975 2.325ZM9.75 12.6Z"
+						/></svg
+					>
+				{/if}
 			</div>
 			<button class="login-button" aria-label="Login Button" type="submit"
 				><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
@@ -87,6 +122,12 @@
 		.background {
 			display: none !important;
 		}
+	}
+	.togglePass {
+		position: absolute;
+		cursor: pointer;
+		right: 0;
+		color: var(--disabled_text);
 	}
 	section {
 		display: grid;
@@ -134,6 +175,13 @@
 		align-items: center;
 		justify-content: center;
 		vertical-align: middle;
+		transition: all 0.3s ease-in-out;
+	}
+	.back-btn a > svg {
+		transition: translate 0.3s ease-in-out;
+	}
+	.back-btn a:hover > svg {
+		translate: -5px;
 	}
 
 	#back-btn-log a:active,
@@ -202,6 +250,9 @@
 		display: flex;
 		align-items: center;
 		justify-content: space-evenly;
+	}
+	.input-password {
+		position: relative;
 	}
 
 	.admin_email,
